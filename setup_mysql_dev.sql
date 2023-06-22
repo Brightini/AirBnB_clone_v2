6f9745742137909941054feb8570b8b7fbc99729
Brightini <brightiniabasi@gmail.com>
2023-06-22 08:57:41 +0100

    Added password

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -5,5 +5,5 @@ CREATE DATABASE IF NOT EXISTS performance_schema;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant user permissions
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'root';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'root';
