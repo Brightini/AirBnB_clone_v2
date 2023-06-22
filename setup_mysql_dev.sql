a21351c814905c10b397abeeb1e67fa7c45e2b1f
Brightini <brightiniabasi@gmail.com>
2023-06-22 08:58:56 +0100

    Deleted db

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,8 @@
 -- This script prepares a MySQL server
 
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
-CREATE DATABASE IF NOT EXISTS performance_schema;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
 -- Grant user permissions
-GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'root';
-GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost' IDENTIFIED BY 'root';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
