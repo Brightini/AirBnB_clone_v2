01670adcc98ca885caf3aed371225b0850107d9a
Brightini <brightiniabasi@gmail.com>
2023-07-15 03:33:23 -0700

    Fixed pycodestyle error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,24 +2,23 @@ from fabric.api import local
 from datetime import datetime
 import os
 
+
 def do_pack():
-	# Get the cuurent date and time
-	now = datetime.now().strftime("%Y%m%d%H%M%S")
+    # Get the cuurent date and time
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
 
-	# Create the versions folder if it doesn't exist
-	if not os.path.exists("versions"):
-		os.makedirs("versions")
+    # Create the versions folder if it doesn't exist
+    if not os.path.exists("versions"):
+        os.makedirs("versions")
 
-	# Set the name of the archive file
-	archive_name = "web_static_" + now + ".tgz"
+    # Set the name of the archive file
+    archive_name = "web_static_" + now + ".tgz"
 
-	# Create the archive by compressing the web_static folder
-	result = local("tar -czvf versions/{} web_static".format(archive_name))
+    # Create the archive by compressing the web_static folder
+    result = local("tar -czvf versions/{} web_static".format(archive_name))
 
-	# Check if the archive was created successfully
-	if result.succeeded:
-		# Return the path of the created archive
-		return "version/" + archive_name
-	else:
-		# Return None if the archive creation failed
-		return None
+    # Check if the archive was created successfully
+    if result.succeeded:
+        return "version/" + archive_name
+    else:
+        return None
