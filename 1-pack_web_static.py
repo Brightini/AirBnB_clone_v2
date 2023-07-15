3ed15faa056a34fc9e67c65a76e5bf4fd5ebe01d
Brightini <brightiniabasi@gmail.com>
2023-07-15 03:26:18 -0700

    Initial commit

---
+++ 1-pack_web_static.py
@@ -0,0 +1,25 @@
+from fabric.api import local
+from datetime import datetime
+import os
+
+def do_pack():
+	# Get the cuurent date and time
+	now = datetime.now().strftime("%Y%m%d%H%M%S")
+
+	# Create the versions folder if it doesn't exist
+	if not os.path.exists("versions"):
+		os.makedirs("versions")
+
+	# Set the name of the archive file
+	archive_name = "web_static_" + now + ".tgz"
+
+	# Create the archive by compressing the web_static folder
+	result = local("tar -czvf versions/{} web_static".format(archive_name))
+
+	# Check if the archive was created successfully
+	if result.succeeded:
+		# Return the path of the created archive
+		return "version/" + archive_name
+	else:
+		# Return None if the archive creation failed
+		return None
