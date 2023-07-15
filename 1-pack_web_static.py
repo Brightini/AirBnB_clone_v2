202ac51132e6e66078daa44e364cf105867ba58a
Brightini <brightiniabasi@gmail.com>
2023-07-15 03:42:00 -0700

    Fixed documentation error

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Fabric script that generates a .tgz archive """"
+""" Fabric script that generates a .tgz archive """
 
 from fabric.api import local
 from datetime import datetime
@@ -7,9 +7,9 @@ import os
 
 
 def do_pack():
-    """ generates a .tgz archive from the contents
-        of the web_static folder
-    """
+    """ generates a .tgz archive from the contents""" \
+        """of the web_static folder"""
+
     # Get the cuurent date and time
     now = datetime.now().strftime("%Y%m%d%H%M%S")
 
